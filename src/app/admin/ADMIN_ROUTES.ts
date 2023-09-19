21b67187ea39f5e7b4b4de128992bdb957645ecf
Roger Miret <r.mir.min@gmail.com>
2023-09-19 13:15:44 +0200

    feat(admin_routes): remove register path

--- src/app/admin/ADMIN_ROUTES.ts
+++ src/app/admin/ADMIN_ROUTES.ts
@@ -9,7 +9,7 @@ export const ADMIN_ROUTES = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: '', loadComponent: () => import('./admin.component').then(m=>m.AdminComponent), children: [
             { path: 'login', loadComponent: () => import('./login/login.component').then(m=>m.LoginComponent), ...canActivate(() => redirectLoggedInTo('admin/add-content')) },
-            { path: 'register', loadComponent: () => import('./register/register.component').then(m=>m.RegisterComponent), ...canActivate(() => redirectLoggedInTo('admin/add-content'))},
+            // { path: 'register', loadComponent: () => import('./register/register.component').then(m=>m.RegisterComponent), ...canActivate(() => redirectLoggedInTo('admin/add-content'))},
             { path: 'add-content', loadComponent: () => import('./admin-add-content/admin-add-content.component').then(m=>m.AdminAddContentComponent), canDeactivate: [canDeactivateGuard] }
         ]}
 ];
